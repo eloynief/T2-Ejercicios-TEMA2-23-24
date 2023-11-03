fbfa5add41877fe475f4026bd46907e5b80e21fd
enieves <enieves@106-02.info2>
2023-11-03 14:40:30 +0100

    twest

--- src/bucles/Ejercicio01.java
+++ src/bucles/Ejercicio01.java
@@ -10,63 +10,73 @@ public class Ejercicio01 {
 		Scanner sc=new Scanner(System.in);
 		
 
-		// variable para guardar un número entero
-		int num, uni, dec, cent, mill;
+		// variables para guardar la hora, minuto, segundo y segundos para incrementar
+		int hora,min,sec,incrementar;
 		
+		
+		//try de horas
 		
 		// Le decimos al usuario que introduzca un número
-		System.out.print("Introduce el número: ");
+		System.out.print("Introduce las horas: ");
 
 		
-		// Comando para introducir el primer número
-		num = sc.nextInt();
-		
-		
-		//
-		uni = num % 10;
-		dec = num / 10;
-		cent = num / 100;
-		mill = num / 1000;
+		// Comando para introducir la hora
+		hora = sc.nextInt();
 
 		
-		//
-		if (num >= 0 && num < 10) {
+		//try de minutos
+		
+		// Le decimos al usuario que introduzca un número
+		System.out.print("Introduce los minutos: ");
+
+		
+		// Comando para introducir los min
+		min = sc.nextInt();
+		
+		//try de segundos
+
+		// Le decimos al usuario que introduzca un número
+		System.out.print("Introduce los segundos: ");
+
+		
+		// Comando para introducir los sec
+		sec = sc.nextInt();
+		
+
+		//try de la cantidad de segundos a incrementar
+		
+		// Le decimos al usuario que introduzca la cantidad de segundos que se van a incrementar
+		System.out.print("Introduce el número de segundos a incrementar: ");
+
+		
+		// Comando para introducir el incremento de las numros
+		incrementar = sc.nextInt();
+		
+		
+		
+		//se aumenta la cantidad de segundos
+		sec+=incrementar;
+		
+		//si los valores son válidos
+
+			//si el numero de segundos supera 59, aumenta la cantidad de minutos y se reduce en 60 la de segundos 
+			while(sec>=60) {
+				min+=1;
+				sec-=60;
+			}
+			
+			//si el numero de minutos supera 59, aumenta la cantidad de horas y se reduce en 60 la de segundos 
+			while(min>=60) {
+				hora+=1;
+				min-=60;
+			}
 			
 			
-			System.out.print("El número introducido es capicúo");
-			
-			
-		} else if (num >= 10 && num < 100 && dec % 10 == uni) {
-			
-			
-			System.out.print("El número introducido es capicúo");
-			
-			
-		} else if (num >= 100 && num < 1000 && cent % 10 == uni) {
-			
-			
-			System.out.print("El número introducido es capicúo");
-			
-			
-		} else if (num >= 1000 && num < 10000 && mill % 10 == uni && cent % 10 == dec % 10) {
-			
-			
-			System.out.print("El número introducido es capicúo");
-			
-			
-		} else if (num < 0 || num >= 10000) {
-			
-			
-			System.out.print("El número introducido no es válido");
+			//imprime la hora
+			System.out.println(hora+":"+min+":"+sec);
 			
 		
-		} 
-		//
-		else {
-			
-			
-			System.out.print("El número introducido no es capicúo");
-		}
+		
 		
 		
 		
